f5055cbdfbe9bd83450b963233b35f4c116c8063
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-10-30 15:25:38 -0400

    feat: Implement user form with validation and styling; disable Angular CLI analytics

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
 import { Component, signal } from '@angular/core';
+import { email, Field, form, required, submit } from '@angular/forms/signals';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Field],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('signal-forms-demo');
+  userModel = signal({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    age: 0,
+  });
+
+  userForm = form(this.userModel, (p) => {
+    required(p.firstName, { message: 'First name is required' });
+    required(p.lastName, { message: 'Last name is required' });
+    required(p.email, { message: 'Email is required' });
+    email(p.email, { message: 'Email must be a valid email address' });
+    required(p.phone, { message: 'Phone is required' });
+    required(p.age, { message: 'Age is required' });
+  });
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    submit(this.userForm, async () => {
+      // Perform login logic here
+      const user = this.userModel();
+      console.log('User:', user);
+      // e.g., await this.userService.createUser(user);
+    });
+  }
 }
