fcac4c46cd0a023a7557de226910fe1a6c577ff1
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-11-20 00:02:55 -0500

    feat: adds angular 21 support

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import {provideZoneChangeDetection, provideZonelessChangeDetection} from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]})
+bootstrapApplication(App, {...appConfig, providers: [provideZonelessChangeDetection(), ...appConfig.providers]})
   .catch((err) => console.error(err));
